97c9213dba4e046a8b6b584b79cabc9a46651f72
WesleyWayneOlivier <WesleyOlivier443@gmail.com>
2024-01-09 12:38:23 +0200

    End of section 7

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Skinet';
+  message = 'You are worth more than you know';
+}
